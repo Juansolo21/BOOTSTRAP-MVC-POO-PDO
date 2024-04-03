202c8ce09e40c7b647bac349ba7ff84c3d3bb9b8
Juansolo21 <137222805+Juansolo21@users.noreply.github.com>
2024-04-02 18:01:31 -0600

    cambios
    locales

--- view/head/head.php
+++ view/head/head.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php
         echo (empty($_GET['id'])) 
-        ? ((strpos($_SERVER['REQUEST_URI'],'create')) ? "Agregando nuevo usuario" : "Index")
+        ? ((strpos($_SERVER['REQUEST_URI'],'create')) ? "Agregando nueva empresa" : "Index")
         : ((strpos($_SERVER['REQUEST_URI'],'show')) ? "Detalles del registro ".$_GET['id'] : "Actualizar registro ".$_GET['id'] );
     ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
@@ -14,7 +14,7 @@
     <div class="container-fluid bg-dark p-2 mb-3">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/proyecto/index.php">Inicio</a>
+            <a class="navbar-brand" href="/BOOTSTRAP-MVC-POO-PDO/index.php">Inicio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -25,8 +25,8 @@
                     Username
                 </a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="/proyecto/view/username/index.php">Usuarios</a></li>
-                    <li><a class="dropdown-item" href="/proyecto/view/username/create.php">Agregar nuevos usuarios</a></li>
+                    <li><a class="dropdown-item" href="/BOOTSTRAP-MVC-POO-PDO/view/username/index.php">Empresas</a></li>
+                    <li><a class="dropdown-item" href="/BOOTSTRAP-MVC-POO-PDO/view/username/create.php">Agregar nueva empresa</a></li>
                 </ul>
                 </li>
             </ul>
